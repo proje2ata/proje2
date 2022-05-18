493ba8067e50cccfa00b63670dd85df511638261
0xberkay <o200706008@gmail.com>
2022-05-18 12:56:14 +0300

    kayit girise js eklendi

---
+++ js/login.js
@@ -0,0 +1,36 @@
+let users = [
+  {
+    username: "admin",
+    password: "admin",
+  },
+];
+
+function login() {
+  let username = document.getElementById("uname").value;
+  let password = document.getElementById("psw").value;
+
+  let user = users.find(function (user) {
+    return user.username === username && user.password === password;
+  });
+
+  if (user) {
+    alert("Giriş başarılı");
+    window.location = "index.html";
+  } else {
+    alert("Kullanıcı adı veya şifre hatalı");
+  }
+}
+
+function register() {
+  let username = document.getElementById("unamer").value;
+  let password = document.getElementById("pswr").value;
+  let user = users.find(function (user) {
+    return user.username === username && user.password === password;
+  });
+  if (user) {
+    alert("Bu kullanıcı adı kullanılıyor");
+  } else {
+    users.push({ username, password });
+    alert("Kayıt başarılı");
+  }
+}
