e1d985ec973cbb2cf8476a082f42f66f04c30b7d
0xberkay <o200706008@gmail.com>
2022-05-18 12:26:08 +0300

    esc tuşu ile modalden çıkma eklendi

--- js/place.js
+++ js/place.js
@@ -15,6 +15,19 @@ function openMenuModal(){
   document.getElementById("menuModal").style.display = "block"
 }
 
+document.onkeydown = function(evt) {
+  evt = evt || window.event;
+  var isEscape = false;
+  if ("key" in evt) {
+      isEscape = (evt.key === "Escape" || evt.key === "Esc");
+  } else {
+      isEscape = (evt.keyCode === 27);
+  }
+  if (isEscape) {
+      closeModal();
+      closeMenuModal();
+  }
+}
 
 var slideIndex = 1;
 showSlides(slideIndex);
--- js/script.js
+++ js/script.js
@@ -61,13 +61,11 @@ function scrollFunction() {
 }
 
 
-// Get the modal
 var modal = document.getElementById('id01');
 
 var model2 = document.getElementById('id02');
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+document.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
